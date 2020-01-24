56fa9e87f56a8432252374b9a43ff4de1d813d76
Pjeferson <paulojeferson34@gmail.com>
2020-01-24 14:06:58 -0300

    Controlling form with unform

--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 // import { Container } from './styles';
 
 import logo from '~/assets/logo.svg';
 
 export default function SingUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <form action="">
-        <input placeholder="Nome" />
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha secreta" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
         <button type="submit">Criar conta</button>
         <Link to="/">Login</Link>
-      </form>
+      </Form>
     </>
   );
 }
