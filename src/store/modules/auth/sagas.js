325c62b53de1c8f373cfa53e0f62f53bf6a7341b
Pjeferson <paulojeferson34@gmail.com>
2020-01-25 17:11:18 -0300

    Configuring error displaying

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import api from '~/services/api';
 import history from '~/services/history';
@@ -17,13 +18,15 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.error('Usuário não é prestador');
+      toast.error('Usuário não é prestador.');
+      yield put(signFailure());
       return;
     }
     yield put(signInSuccess(token, user));
 
     history.push('/dashboard');
   } catch (error) {
+    toast.error('Falha na autenticação. Verifique seus dados.');
     yield put(signFailure());
   }
 }
