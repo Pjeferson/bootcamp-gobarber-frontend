b15bd20d091998729b5ba7e13c23f25ae0eaad4b
Pjeferson <paulojeferson34@gmail.com>
2020-01-25 16:38:50 -0300

    Configuring redux states persistence

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,11 @@
+import { persistStore } from 'redux-persist';
 import { createStore, compose, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
 import reducers from './modules/rootReducer';
 import sagas from './modules/rootSaga';
+
+import persistReducers from './persistReducers';
 
 const middlewares = [];
 
@@ -20,8 +23,10 @@ const composer =
     ? compose(applyMiddleware(...middlewares), console.tron.createEnhancer())
     : compose(applyMiddleware(...middlewares));
 
-const store = createStore(reducers, composer);
+const store = createStore(persistReducers(reducers), composer);
+
+const persistor = persistStore(store);
 
 sagaMiddleware.run(sagas);
 
-export default store;
+export { store, persistor };
